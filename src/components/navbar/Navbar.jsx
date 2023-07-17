ccba29352ca02686319c1d255667d1dc9e025fa6
Ryan <phoen.ryan@gmail.com>
2023-07-18 00:01:15 +0800

    small fixes to responsiveness

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import {RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import './navbar.css';
 import logo from '../../assets/logo.svg';
 
